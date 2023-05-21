28f6005f3e4e0af981278d0a4a6585808c39f28a
richard <richard5amc@gmail.com>
2023-05-21 17:09:45 +0200

    abcdef

--- rec.py
+++ rec.py
@@ -4,15 +4,17 @@ def client_program():
     server_ip = "192.168.178.23"
     server_port = 65432
 
+    print("Trying to connect to server...")
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_ip, server_port))
 
     message = input(" -> ")
+    print("_______________________")
 
     while message.lower().strip() != "bye":
         client_socket.send(message.encode())
         data = client_socket.recv(1024).decode()
-        print("Received from server: " + data)
+        print("<-- " + data)
         message = input(" -> ")
 
     client_socket.close()
