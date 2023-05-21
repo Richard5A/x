e2725925cd6b8da2266a3db2a411683199a46703
richard <richard5amc@gmail.com>
2023-05-21 11:33:54 +0200

    abcdef

--- receaver2.py
+++ receaver2.py
@@ -1,9 +1,8 @@
-import time
 import socket
 
 HOST = '10.10.11.208'  # The server's hostname or IP address
 ME = '10.10.14.46'
-PORT = 65432        # The port used by the server
+PORT = 65432  # The port used by the server
 PORT_R = 65434
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -24,14 +23,12 @@ while True:
         s.sendall(bytes(inp, 'UTF-8'))
         print("Command sent")
         s.close()
-    
-    
-        
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((ME, PORT_R))
         print("Listening..")
         s.listen()
-        conn, addr = s.accept()
+        conn, _ = s.accept()
         with conn:
             data = conn.recv(1024)
             print(data.decode())
